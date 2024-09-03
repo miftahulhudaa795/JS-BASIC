261af538fe3e74f35b767945f5591b81f02e8654
miftahulhudaa795 <miftahulhuda0696@gmail.com>
2024-09-03 23:21:21 +0700

    Tugas-1.js

---
+++ Tugas-1.js
@@ -0,0 +1,24 @@
+//1. Tipe Data
+let a = 42; //Number
+let b = 'Hello, World!'; //String
+let c = true;  //Boolean
+let d = null;  //Object/Null
+let e = underfined; //Undefined
+let f = {name: 'Alice', age: 25}; //Object
+let g = [1, 2, 3, 4]; //Object
+let h = function() { return 'Hello!'; };  //Function
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+
+//2. Output = 141
+let num = '123';
+let result = Number(num) + 18;
+console.log(result); 
+
